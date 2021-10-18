1e7ef230c71c2957d26f33dfa8c7c827b2a44a4c
nandinimalik16 <nandinimalik2@gmail.com>
2021-10-18 21:23:18 +0530

    Updated

---
+++ Dynamic Programming/minin.cpp
@@ -0,0 +1,54 @@
+// Minimum number of insertion in a string to make it a palindrome
+//using top-down approach
+//y=revserse(a)
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int lps(string x,string y,int n)
+{
+	int t[n+1][n+1],i,j;
+	
+	for(i=0;i<=n;i++)
+	{
+		t[0][i]=t[i][0]=0;
+	}
+	
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(x[i-1]==y[j-1])
+			{
+				t[i][j]=1+t[i-1][j-1];
+				
+			}
+			else
+			{
+				t[i][j]=max(t[i-1][j],t[i][j-1]);
+			}
+		}
+	}
+
+	return n-t[n][n];
+}
+
+int main()
+{
+	string x,y;
+	int n,m;
+	cout<<"Enter string=";
+	cin>>x;
+	y=x;
+	reverse(x.begin(),x.end());
+	n=x.length();
+	
+	int l=lps(x,y,n);
+	cout<<"Number of insertions="<<l;
+
+	return 0;
+}
+
+
+
