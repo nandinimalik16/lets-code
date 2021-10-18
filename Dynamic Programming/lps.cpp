b42c5671d798aa3b6fabff0d0cfc5f62728c3d96
nandinimalik16 <nandinimalik2@gmail.com>
2021-10-18 14:57:32 +0530

    Updated

---
+++ Dynamic Programming/lps.cpp
@@ -0,0 +1,57 @@
+//longest palindromic subsequence
+//using top down approach
+//lps(a)=lcs(a,reverse(a))
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int lps(string x,string y,int n,int m)
+{
+	int t[n+1][m+1],i,j;
+	
+	for(i=0;i<=n;i++)
+	{
+		t[i][0]=0;
+	}
+	for(i=1;i<=m;i++)
+	{
+		t[0][i]=0;
+	}
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=m;j++)
+		{
+			if(x[i-1]==y[j-1])
+			{
+				t[i][j]=1+t[i-1][j-1];
+				
+			}
+			else
+			{
+				t[i][j]=max(t[i-1][j],t[i][j-1]);
+			}
+		}
+	}
+
+	return t[n][m];
+}
+
+int main()
+{
+	string x,y;
+	int n,m;
+	cout<<"Enter string=";
+	cin>>x;
+	y=x;
+	reverse(x.begin(),x.end());
+	n=x.length();
+	m=y.length();
+	int l=lps(x,y,n,m);
+	cout<<"Length of longest palindromic subsequence="<<l;
+
+	return 0;
+}
+
+
+
