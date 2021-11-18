3ca438acba26c78a21bc304098355aede6adc514
nandinimalik16 <nandinimalik2@gmail.com>
2021-11-18 09:28:52 +0530

    Updated

---
+++ tree/Q5.cpp
@@ -0,0 +1,36 @@
+// Populating next right pointers in each node
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node/submissions/
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root && root->left!=NULL)
+        {
+            root->left->next=root->right;
+            connect(root->left);
+            if(root->next!=NULL)
+            root->right->next=root->next->left;
+            connect( root->right);
+        }
+        return root;
+    }
+};
