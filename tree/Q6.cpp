b1259fd509780256137cbc48e4f17b6ac6687a62
nandinimalik16 <nandinimalik2@gmail.com>
2021-11-18 10:44:48 +0530

    Updated

---
+++ tree/Q6.cpp
@@ -0,0 +1,36 @@
+// Merge two Binary trees
+// https://leetcode.com/problems/merge-two-binary-trees/submissions/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(root1==NULL && root2==NULL)
+        {
+            return NULL;
+        }
+        if (root1== NULL)
+        {
+            return root2;
+        }
+        if(root2==NULL)
+        {
+            return root1;
+        }
+        TreeNode *tmp=new TreeNode(root1->val+root2->val);
+        tmp->left=mergeTrees(root1->left,root2->left);
+        tmp->right=mergeTrees(root1->right,root2->right);
+        return tmp;
+    }
+};
