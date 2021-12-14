cf1cb8e61ad7c54553877e8fdf8f28a49847bcab
nandinimalik16 <nandinimalik2@gmail.com>
2021-12-14 12:56:06 +0530

    Added

---
+++ array/largPerimeter.cpp
@@ -0,0 +1,20 @@
+// Largest Perimeter
+// https://leetcode.com/problems/largest-perimeter-triangle/submissions/
+
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=n-1;i>=2;i--)
+        {
+            if(nums[i]+nums[i-1]<=nums[i-2]||nums[i]+nums[i-2]<=nums[i-1]||nums[i-2]+nums[i-1]<=nums[i])
+            {
+                continue;
+            }
+            return nums[i]+nums[i-1]+nums[i-2];
+        }
+        return 0;
+    }
+};
